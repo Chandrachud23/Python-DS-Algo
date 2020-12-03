fb64f6eeea4d23099b3943eb00aaa2323719baf0
Chandrachud Pati <75125871+Chandrachud23@users.noreply.github.com>
2020-12-03 22:27:39 +0530

    Create Insertion Sort.py
    Sample output-
    [9,12,23,34,45,53,84]

---
+++ Arrays/Sorting/Insertion Sort.py
@@ -0,0 +1,20 @@
+def InsertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while key < arr[j] and j >= 0: 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+  
+  
+# Driver code to test above 
+arr = [23,45,53,12,34,9,84] 
+InsertionSort(arr)
