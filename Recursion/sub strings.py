f9fbdda5e3505e2ccf2d914905ed90290779733a
Chandrachud Pati <75125871+Chandrachud23@users.noreply.github.com>
2020-12-30 01:14:38 +0530

    Create sub strings.py
    C
    B
    BC
    A
    AC
    AB
    ABC

---
+++ Recursion/sub strings.py
@@ -0,0 +1,15 @@
+def lst_substrings(s,ans):
+    if len(s) == 0:
+        print(ans)
+        return
+    ch=s[0]
+    ros=s[1:]
+    
+    lst_substrings(ros,ans)
+    lst_substrings(ros,ans+ch)
+
+s="ABC"
+ans=""
+lst_substrings(s,ans)
+
+Watch this for better explanation: https://www.youtube.com/embed/j9RG18jfnRY?list=PLfqMhTWNBTe0b2nM6JHVCnAkhQRGiZMSJ
